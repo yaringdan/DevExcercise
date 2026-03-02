bd1f96fd873e3ce61741d2be2275ff0142f27927
yarin <yaringdan@gmail.com>
2026-03-02 19:08:44 +0200

    add formatter

---
+++ Formatters/terminal_formatter.py
@@ -0,0 +1,14 @@
+from formatter import Formatter
+
+class TerminalFormatter(Formatter):
+    def show(self, stats, total):
+        for field, counts in stats.items():
+            print(f"{field.capitalize()}:")
+            
+            sorted_data = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+            
+            for value, count in sorted_data:
+                percentage = (count / total) * 100
+                print(f"{value} {percentage:.2f}%")
+            
+            print()
